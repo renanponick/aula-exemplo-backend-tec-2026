b086ba4b5dcf44cb82a3c55d6637479ab8b39cc2
Renan <renan.ponick@prof.sc.senac.br>
2026-07-06 21:25:30 -0300

    1: aula get

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express from 'express'
+import bancoDeDados from './repository/index.js'
 
-// server ou app
 const app = express()
-//- path params - router
-app.get("/api/v1/somar", (req, res) => {
-    // query params
-    console.log(req.query)
-
-    const num1 = Number(req.query.num1)
-    const num2 = Number(req.query.num2)
-    const resultado = num1 + num2
-
-    res.send({ resultado, "Tarefa": "Pesquise sobre QueryParams" })
+//buscar
+app.get("/api/pessoa/:id", (req, res) => {
+    const id = req.params.id
+    const pessoa = bancoDeDados.find(it => it.id == id)
+    if (!pessoa) {
+        res.send({ message: "Pessoa não encontrada" })
+        return
+    }
+    res.send({ pessoa })
+})
+//criar
+app.get("/api/pessoa", (req, res) => {
+    const { id, name } = req.query
+    if(!id || !name) {
+        res.send({ message: "Favor informar id e name" })
+        return 
+    }
+    bancoDeDados.push({ id, name })
+    res.send({ message: "Pessoa criada com sucesso" })
 })
 
-// aqui em diante
-
-// metodo GET da função de subtrair usando QueryParam
-// Com numero1 e numero2 ou num1 e num2
-
 app.listen(3000, () => {
-    console.log("Servidor ouvindo na porta 3000")
+    console.log("Servidor escutando na porta 3000")
 })
