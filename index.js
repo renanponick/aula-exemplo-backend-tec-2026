2028a2a5a01b9b340d2ebc2179ca3d06f98fce77
Renan <renan.ponick@prof.sc.senac.br>
2026-07-16 19:41:01 -0300

    4 aula router

--- index.js
+++ index.js
@@ -1,48 +1,51 @@
 import express from 'express'
+import cachorro 
+    from './router/cachorro.js'
 
 const app = express()
 
 app.use(express.json())
 
-app.post("/api/v1/somar", (req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = Number(num1) + Number(num2)
+app.use('/api/v1/cachorro', cachorro)
 
-    res.status(200).send({
-        message: resultado
-    })
-})
+// app.post("/api/v1/somar", (req, res) => {
+//     const { num1, num2 } = req.body
+//     const resultado = Number(num1) + Number(num2)
 
-// subtrair
-app.post("/api/v1/subtrair", (req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = Number(num1) - Number(num2)
+//     res.status(200).send({
+//         message: resultado
+//     })
+// })
 
-    res.status(200).send({
-        message: resultado
-    })
-})
+// // subtrair
+// app.post("/api/v1/subtrair", (req, res) => {
+//     const { num1, num2 } = req.body
+//     const resultado = Number(num1) - Number(num2)
 
+//     res.status(200).send({
+//         message: resultado
+//     })
+// })
 
-// mult
-app.get("/api/v1/multiplicar", (req, res) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) * Number(num2)
+// // mult
+// app.get("/api/v1/multiplicar", (req, res) => {
+//     const { num1, num2 } = req.query
+//     const resultado = Number(num1) * Number(num2)
 
-    res.status(200).send({
-        message: resultado
-    })
-})
+//     res.status(200).send({
+//         message: resultado
+//     })
+// })
 
-// div
-app.get("/api/v1/dividir", (req, res) => {
-    const { num1, num2 } = req.query
-    const resultado = Number(num1) / Number(num2)
+// // div
+// app.get("/api/v1/dividir", (req, res) => {
+//     const { num1, num2 } = req.query
+//     const resultado = Number(num1) / Number(num2)
 
-    res.status(200).send({
-        message: resultado
-    })
-})
+//     res.status(200).send({
+//         message: resultado
+//     })
+// })
 
 app.listen(3000, () => {
     console.log("Servidor escutando na porta 3000")
