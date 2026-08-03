ff434a0910eda579d9800d07ad61d316db726fe5
Renan <renan.ponick@prof.sc.senac.br>
2026-08-03 20:14:55 -0300

    init repo

--- index.js
+++ index.js
@@ -1,51 +1,10 @@
 import express from 'express'
-import cachorro 
-    from './router/cachorro.js'
+import cachorro from './router/cachorro.js'
 
 const app = express()
-
 app.use(express.json())
 
 app.use('/api/v1/cachorro', cachorro)
-
-// app.post("/api/v1/somar", (req, res) => {
-//     const { num1, num2 } = req.body
-//     const resultado = Number(num1) + Number(num2)
-
-//     res.status(200).send({
-//         message: resultado
-//     })
-// })
-
-// // subtrair
-// app.post("/api/v1/subtrair", (req, res) => {
-//     const { num1, num2 } = req.body
-//     const resultado = Number(num1) - Number(num2)
-
-//     res.status(200).send({
-//         message: resultado
-//     })
-// })
-
-// // mult
-// app.get("/api/v1/multiplicar", (req, res) => {
-//     const { num1, num2 } = req.query
-//     const resultado = Number(num1) * Number(num2)
-
-//     res.status(200).send({
-//         message: resultado
-//     })
-// })
-
-// // div
-// app.get("/api/v1/dividir", (req, res) => {
-//     const { num1, num2 } = req.query
-//     const resultado = Number(num1) / Number(num2)
-
-//     res.status(200).send({
-//         message: resultado
-//     })
-// })
 
 app.listen(3000, () => {
     console.log("Servidor escutando na porta 3000")
--- router/cachorro.js
+++ router/cachorro.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import somar from '../service/cachorro.js'
 
 const router = express.Router()
-
 router.post("/somar", (req, res) => {
     const { num1, num2 } = req.body
-    const resultado = Number(num1) + Number(num2)
+    const resultado = somar(Number(num1), Number(num2))
 
     res.status(200).send({
         message: resultado
