b86bf1578ceff2d8ab094b5d1fbcfd900bd8c71b
Renan <renan.ponick@prof.sc.senac.br>
2026-08-03 22:02:18 -0300

    done

--- router/cachorro.js
+++ router/cachorro.js
@@ -1,14 +1,30 @@
 import express from 'express'
-import somar from '../service/cachorro.js'
+import { somar, dividir } from '../service/cachorro.js'
 
 const router = express.Router()
-router.post("/somar", (req, res) => {
-    const { num1, num2 } = req.body
-    const resultado = somar(Number(num1), Number(num2))
 
-    res.status(200).send({
-        message: resultado
-    })
+router.post("/somar", (req, res) => {
+    try {
+        const { num1, num2 } = req.body
+        const resultado = somar(Number(num1), Number(num2))
+    
+        res.status(200).send({
+            message: resultado
+        })
+    } catch (error) {
+        res.status(500).send({ messsage: error.message })
+    }
+})
+
+router.post("/dividir", (req, res) => {
+    try {
+        const { num1, num2 } = req.body
+        const resultado = dividir(num1, num2)
+
+        res.status(200).send({ message: resultado })
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
 })
 
 export default router
--- service/cachorro.js
+++ service/cachorro.js
@@ -1,5 +1,17 @@
-function somar(num1, num2) {
+export function somar(num1, num2) {
+    if(isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar numeros")
+    }
     return num1 + num2
 }
 
-export default somar
+export function dividir(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        throw new Error("Favor informar somente números")
+    } 
+    if (num2 == 0) {
+        throw new Error("Não é possível dividir por zero")
+    }
+
+    return num1 / num2
+}
